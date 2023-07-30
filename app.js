a8f7bf7583c7be07c9d48ed1cc43272744ffff42
Samuuular <samuellaredo6@gmail.com>
2023-07-29 21:38:19 -0300

    Mercadoliebre responsive con formulario register

--- app.js
+++ app.js
@@ -10,4 +10,6 @@ app.listen(PORT, () => console.log('Servidor sprinteando en el puerto: ' + PORT)
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
 
+app.get("/register.html", (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
 
+app.get("/login.html", (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
